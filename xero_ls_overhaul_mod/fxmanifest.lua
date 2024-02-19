c1f00c5400960211dbff865b78859b96af3ebc71
xero <101067357+WXero@users.noreply.github.com>
2024-02-19 22:16:05 +0100

    1.0

---
+++ xero_ls_overhaul_mod/fxmanifest.lua
@@ -0,0 +1,17 @@
+this_is_a_map 'yes'
+fx_version 'adamant'
+games {'gta5'}
+
+client_script 'client.lua'
+
+files {
+    'stream/downtown_vinewood/los_santos_freeway.ymt',
+}
+
+
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'los_santos_freeway.ymt' --VINEWOOD SCENARIOS
+
+
+-- MADE BY XEEROO 
+
+-- ORIGINAL DOWLOAD https://github.com/WXero/Los-Santos-Overhaul
